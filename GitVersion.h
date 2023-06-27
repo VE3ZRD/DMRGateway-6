9260ef0511498415a04a646a96cd023cea6fc126
phil <ve3rd@rac.ca>
2023-06-27 10:28:18 -0400

    update

--- GitVersion.h
+++
@@ -1,1 +0,0 @@
-const char *gitversion = "cd3b2f00528dfcb4e57f0dabbc87fe6d91e29aaf";
--- Version.h
+++ Version.h
@@ -19,6 +19,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20220517-4RD";
+const char* VERSION = "20230625-5RD";
 
 #endif
